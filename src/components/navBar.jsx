31c5eb5f8d8cd68e0fc6d20e33262138a0ee77e8
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-05-06 19:26:31 +0100

    added the not found component

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <svg
-          class="fill-current h-8 w-8 mr-2"
+          className="fill-current h-8 w-8 mr-2"
           width="54"
           height="54"
           viewBox="0 0 54 54"
@@ -13,7 +13,7 @@ const NavBar = () => {
         >
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg>
-        <span class="font-semibold text-xl tracking-tight">Spectrum</span>
+        <span className="font-semibold text-xl tracking-tight">Spectrum</span>
       </div>
     </nav>
   );
---
+++ src/components/notFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+    return ( 
+        <h1 className="text-6xl text-center mx-auto mt-32"> No Photos Found</h1>
+     );
+}
+ 
+export default NotFound;
