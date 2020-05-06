3eacbaf0362be88331e205e7c9bc02cdc3b42c5b
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-05-06 09:30:24 +0100

    make app display random photos when app is mounted

--- src/App.js
+++ src/App.js
@@ -6,21 +6,43 @@ import ImageSearch from "./components/imageSearch";
 class App extends Component {
   state = {
     photos: [],
-    Searchterm: "",
+    searchTerm: "",
     isLoading: true,
   };
+
   async componentDidMount() {
     const { data } = await axios.get(
-      `https://api.unsplash.com/search/photos?query=lagos&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
+      "https://api.unsplash.com/search/photos?query=random&client_id=i-MziJcxj3BaybCrXFeJNx4knOnS6c-93rnEoPFbHLA"
+    );
+    const photos = data.results
+    this.setState({photos})
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+    const { data } = await axios.get(
+      `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
     );
     const photos = data.results;
     this.setState({ photos });
-  }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    let searchTerm = this.state.searchTerm;
+    searchTerm = input.value;
+    this.setState({ searchTerm });
+  };
 
   render() {
     const { photos } = this.state;
     return (
-      <div className=" container mx-auto">
+      <div className="container mx-auto">
+        <ImageSearch
+          value={this.state.searchTerm}
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+        />
         <div className="grid grid-cols-3 gap-4">
           {photos.map((photo) => (
             <ImageCard key={photo.id} photo={photo} />
