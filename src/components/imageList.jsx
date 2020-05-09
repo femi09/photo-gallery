964a59e66cf56e02a81fef6e96607bc71ab1b766
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-05-09 21:45:46 +0100

    created an ImageList component to clean up app.js

---
+++ src/components/imageList.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import axios from "axios";
+import ImageCard from './imageCard';
+import ImageSearch from './imageSearch';
+import NotFound from "./notFound";
+
+
+class ImageList extends Component {
+  state = {
+    photos: [],
+    searchTerm: "",
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get(
+      `https://api.unsplash.com/search/photos?page=3&per_page=30&query=random&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
+    );
+    const photos = data.results;
+    const isLoading = false;
+    this.setState({ photos, isLoading });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+    const { data } = await axios.get(
+      `https://api.unsplash.com/search/photos?page=5&per_page=30&query=${searchTerm}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
+    );
+    const photos = data.results;
+
+    this.setState({ photos });
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    let searchTerm = this.state.searchTerm;
+    searchTerm = input.value;
+    this.setState({ searchTerm });
+  };
+
+  render() {
+    const { photos, isLoading } = this.state;
+    return (
+      <div className="container mx-auto">
+        <ImageSearch
+          value={this.state.searchTerm}
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+        />
+        {!isLoading && photos.length === 0 && <NotFound photos={photos} />}
+        <div className="grid grid-cols-3 gap-4">
+          {photos.map((photo) => (
+            <ImageCard key={photo.id} photo={photo} />
+          ))}
+        </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default ImageList;
