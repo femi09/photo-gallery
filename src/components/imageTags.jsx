ce6b7e476c2875166cad98c5532abebad543db8d
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-05-12 09:41:27 +0100

    added the scroll effect to the tag list and hid the scroll bar for diff browsers

--- src/components/imageTags.jsx
+++ src/components/imageTags.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ImageTags = ({ photoTags, onClick }) => {
-  return (
+const ImageTags = ({ photoTags, onClick}) => {
+ return (
     <div className="w-full bg-teal-500">
-      <div className="flex text-white hover:text-grey-600 flex-row ml-6">
-        <ul className="w-full  flex center">
+      <div className="flex  text-white flex-row">
+        <ul className="flex tag .disable-scrollbars overflow-x-auto flex center p-3">
           {
             photoTags.map((photoTag, index) => 
-            <li key={index} onClick={onClick} className="mr-6 cursor-pointer">{photoTag}</li>
+            <li key={index} onClick={onClick} className="flex flex-none font-normal px-8 hover:font-bold cursor-pointer">{photoTag}</li>
             )}
           
         </ul>
