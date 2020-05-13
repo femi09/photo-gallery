6da6b5313529c88282836248c4c5ad38b42878b1
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-05-13 19:29:42 +0100

    adding the active state

--- src/components/imageTags.jsx
+++ src/components/imageTags.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const ImageTags = ({ photoTags, onClick}) => {
+const ImageTags = ({ photoTags, onClick, active}) => {
+
  return (
     <div className="w-full bg-teal-500">
-      <div className="flex  text-white flex-row">
+      <div className="flex text-white flex-row">
         <ul className="flex tag .disable-scrollbars overflow-x-auto flex center p-3">
           {
             photoTags.map((photoTag, index) => 
