a0859e0181c3b3eabcbdb2e23e569330af0a628e
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-08-25 01:40:06 +0100

    master

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import {Link}  from 'react-router-dom'
 
 const NotFound = ({ searchTerm }) => {
   return (
     <div className="text-center">
       <h1 className="text-6xl text-center mx-auto mt-32">
-        No Photos Found for {searchTerm}{" "}
+        No Photos Found For {searchTerm}{" "}
       </h1>
      
     </div>
